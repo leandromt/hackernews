0d9a3ed6b54f55aabd1f86ce774247620c69dfef
Leandro Tavares <leandro.mendestavares@gmail.com>
2018-08-09 23:45:36 -0300

    App JS Map JavaScript

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {
+            list.map(function (item, key) {
+              return (
+                <li id={item.key} key={item.objectID}>
+                  <p><a href={item.ul}>{item.title}</a></p>
+                  <p>{item.author}</p>
+                  <p>{item.num_comments}</p>
+                  <p>{item.points}</p>
+                  <p>{item.objectID}</p>
+                </li>
+              );
+            })
+          }
+        </ul>
       </div>
     );
   }
