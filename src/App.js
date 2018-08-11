c28f60dd0a75166d053664a35bf199c46c9c7cf4
Leandro Tavares <leandro.mendestavares@gmail.com>
2018-08-11 11:22:26 -0300

    Click button

--- src/App.js
+++ src/App.js
@@ -17,26 +17,49 @@ const list = [
     points: 5,
     objectID: 1,
   },
+  {
+    title: 'Teste',
+    url: 'https://github.com/reactjs/teste',
+    author: 'Teste Abramov, Andrew Clark',
+    num_comments: 6,
+    points: 2,
+    objectID: 4,
+  }
 ];
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Olá Mundo!',
+      status: true,
+      list,
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
+  }
+
   render() {
     return (
       <div className="App">
+        <h1>{this.state.title}</h1>
         <ul>
-          {
-            list.map(function (item, key) {
-              return (
-                <li id={item.key} key={item.objectID}>
-                  <p><a href={item.ul}>{item.title}</a></p>
-                  <p>{item.author}</p>
-                  <p>{item.num_comments}</p>
-                  <p>{item.points}</p>
-                  <p>{item.objectID}</p>
-                </li>
-              );
-            })
-          }
+          {this.state.list.map(item =>
+            <li id={item.key} key={item.objectID}>
+              <p><a href={item.ul}>{item.title}</a></p>
+              <p>{item.author}</p>
+              <p>{item.num_comments}</p>
+              <p>{item.points}</p>
+              <p><button onClick={() => this.onDismiss(item.objectID)} type="button">Dismiss</button></p>
+            </li>
+          )}
         </ul>
       </div>
     );
