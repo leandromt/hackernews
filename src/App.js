1f5b71dd93e608fb1c8227d79ad51dd18c7e8da9
Leandro Tavares <leandro.mendestavares@gmail.com>
2018-08-12 17:03:09 -0300

    Components JSX

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import Search from './components/Search';
-import Table from './components/Table';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Table from "./components/Table";
 
 const list = [
   {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
-    objectID: 0,
+    objectID: 0
   },
   {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
+    title: "Redux",
+    url: "https://github.com/reactjs/redux",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
-    objectID: 1,
+    objectID: 1
   },
   {
-    title: 'Teste',
-    url: 'https://github.com/reactjs/teste',
-    author: 'Teste Abramov, Andrew Clark',
+    title: "Teste",
+    url: "https://github.com/reactjs/teste",
+    author: "Teste Abramov, Andrew Clark",
     num_comments: 6,
     points: 2,
-    objectID: 4,
+    objectID: 4
   }
 ];
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
-      title: 'Olá Mundo!',
+      title: "Olá Mundo!",
       status: true,
-      searchTerm: '',
-      list,
+      searchTerm: "",
+      list
     };
 
     this.onSearchChange = this.onSearchChange.bind(this);
