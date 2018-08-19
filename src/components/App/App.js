760ac2b185bb07a0dacbf5b109c66764949d272b
Leandro Tavares <leandro.mendestavares@gmail.com>
2018-08-19 16:16:59 -0300

    Organizando estrutura de pastas

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,24 @@
+// Libs
 import React, { Component } from "react";
 import axios from "axios";
-import Header from "./components/Header";
-import Table from "./components/Table";
-import Button from "./components/Button";
+
+// Style
 import "./App.css";
 
-const DEFAULT_QUERY = "react";
-const DEFAULT_HPP = "100";
+// Components
+import { Header, Table } from "./../Partials";
+import { Button } from "./../Buttons";
 
-const PATH_BASE = "https://hn.algolia.com/api/v1";
-const PATH_SEARCH = "/search";
-const PARAM_SEARCH = "query=";
-const PARAM_PAGE = "page=";
-const PARAM_HPP = "hitsPerPage=";
+// Constants
+import {
+  DEFAULT_QUERY,
+  DEFAULT_HPP,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP
+} from "./../../constants";
 
 class App extends Component {
   _isMounted = false;
--- src/components/Partials/Table.js
+++ src/components/Partials/Table.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Button from "./Button";
+import { Button } from "./../Buttons";
 
 class Table extends Component {
   render() {
